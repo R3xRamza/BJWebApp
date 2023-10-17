83f126ce3f1dbf357df438adc3c5bbf40526226b
R3xRamza <rexramza@gmail.com>
2023-10-17 13:18:20 -0500

    fixbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
-import WordBank from './components/WordBank.jsx';
-import Box from './components/Box.jsx';
 import Hero from './components/startPage.jsx';
 
 const App = () => {
-  const [droppedWords, setDroppedWords] = useState([]);
-
-  const handleDrop = (word) => {
-    setDroppedWords([...droppedWords, word]);
-  };
 
   return (
     <div>
